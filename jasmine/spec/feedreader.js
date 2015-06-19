f5eed311b8111b8d743a7bc3e18b8092b7639da2
Brock Cooper <brockcooper@gmail.com>
2015-06-19 07:48:44 -0600

    Initial commit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,46 +27,43 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* Loops through each feed in the allFeeds object and 
+         * ensures it has a URL defined and that the URL is not empty.
          */
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* Loops through each feed in the allFeeds object and ensures 
+         * it has a name defined and that the name is not empty.
          */
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* This test suite tests the functionality of the menu featured
+     * throughout the application
+     * / 
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
+        /* Ensures the menu element is hidden by default. Analyzes the
+         *  HTML and the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+         /* Ensures the menu changes visibility when the menu icon is clicked. 
+          * This test should have two expectations: 
+          * does the menu display when clicked
+          * and does it hide when clicked again.
           */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* This test suite tests the functionality of the Initial Entries * / 
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+        /* Ensures when the loadFeed function is called and completes its work, 
+         * there is at least a single .entry element within the .feed container.
+         * loadFeed() is asynchronous so this test requires the use of Jasmine's 
+         * beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* This test suite tests the functionality of the New Feed Selection * / 
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+        /* Ensures when a new feed is loaded by the loadFeed function that the 
+         * content actually changes. loadFeed() is also asynchronous.
          */
 }());
